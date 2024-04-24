2464db373408fd76bdc9d077af96da2e23500fbd
yewon-saurus <ywwwon01@gmail.com>
2024-04-24 22:50:43 +0900

    [client] UI/UX: ListItem 초안 작성

--- client/src/pages/List/ListItem.jsx
+++ client/src/pages/List/ListItem.jsx
@@ -1,17 +1,46 @@
-import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+
+import styled from "styled-components";
+
 import useDayKor from "../../shared/hooks/useDayKor";
 
+const ListItemStyled = styled.div`
+display: flex;
+justify-content: space-between;
+margin: 1em 0;
+
+div {
+    padding: 1em;
+}
+
+div span {
+    padding: 1em;
+}
+
+button {
+    width: 10em;
+}
+`;
+
 const ListItem = (props) => {
+    const nav = useNavigate();
+
     const day = useDayKor(props.date); // 무슨 요일이니?
 
     return (
-        <div>
-            <span>
+        <ListItemStyled>
+            <div>
                 <span>{day}</span>
-                <span>{props.rate}</span>
-            </span>
-            <button>수정</button>
-        </div>
+                <span>{
+                props.rate > 0
+                ? '★'.repeat(props.rate)
+                : '-'
+                }</span>
+            </div>
+            <button onClick={() => {
+                nav('/detail/' + props.id);
+            }}>수정</button>
+        </ListItemStyled>
     );
 }
 
