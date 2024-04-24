7cbacd31073f872da5b6c1b0ac217d3fdfa2243d
yewon-saurus <ywwwon01@gmail.com>
2024-04-24 23:19:27 +0900

    [client] feat: Detail page '저장하기' 버튼 클릭 시 navigate 처리, title(Header 문구) 초기화 조치

--- client/src/pages/Detail/ConditionDetail.jsx
+++ client/src/pages/Detail/ConditionDetail.jsx
@@ -1,6 +1,10 @@
+import { useNavigate } from "react-router";
+import { useDispatch } from "react-redux";
+
 import styled from "styled-components";
 
 import RateButton from "./RateButton";
+import { initTitle } from "../../shared/redux/modules/title";
 
 const ConditionDetailStyled = styled.div`
 div {
@@ -15,13 +19,22 @@ button {
 `;
 
 const ConditionDetail = () => {
+    const nav = useNavigate();
+    const dispatch = useDispatch();
+
+    const handleClickSave = () => {
+        // TODO: dispatch the condition modify
+        dispatch(initTitle()); // title(Header 문구) 초기화
+        nav(-1); // 뒤로 가기, List page로 돌아가기
+    }
+
     return (
         <ConditionDetailStyled>
             <div>
                 <span>금</span>
                 <RateButton />
             </div>
-            <button>저장하기</button>
+            <button onClick={handleClickSave}>저장하기</button>
         </ConditionDetailStyled>
     );
 }
