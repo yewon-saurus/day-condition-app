98d9c29f30c185dd87c9b184420f5e0e19d3f813
yewon-saurus <ywwwon01@gmail.com>
2024-04-25 00:06:32 +0900

    [client] UI/UX: Detail page 별점 버튼 초안 작성

--- client/src/pages/Detail/RateButton.jsx
+++ client/src/pages/Detail/RateButton.jsx
@@ -1,17 +1,38 @@
+import { useState } from "react";
+
 import styled from "styled-components";
 
 const RateButtonStyled = styled.div`
 font-size: 2em;
+
+span {
+    cursor: pointer;
+}
 `;
 
 const RateButton = () => {
+    const [rate, setRate] = useState([false, false, false, false, false]);
+
+    const handleClickRate = (idx) => {
+        let dummyRate = new Array(rate); // 깊은 복사. 얕은 복사 시 rate에 직접 수정을 시도하게 되는 것이므로 깊은 복사 처리해야 한다
+        
+        for (let i = 0; i < idx; i++) {
+            dummyRate[i] = true;
+        }
+        for (let i = idx; i < 5; i++) {
+            dummyRate[i] = false;
+        }
+
+        setRate(dummyRate);
+    }
+
     return (
         <RateButtonStyled>
-            <span>★</span>
-            <span>★</span>
-            <span>★</span>
-            <span>★</span>
-            <span>★</span>
+            {
+                rate.map((ele, idx) => (
+                    <span key={idx} onClick={() => handleClickRate(idx + 1)}>{ele ? '★' : '☆'}</span>
+                ))
+            }
         </RateButtonStyled>
     );
 }
